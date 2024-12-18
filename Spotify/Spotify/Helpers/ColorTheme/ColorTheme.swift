9d495a8448e6c0428c092926e0bcdb9643d87e9a
FurkanMDemiray <furkanmdemiray@gmail.com>
2024-12-18 14:33:04 +0300

    added animations to button views

---
+++ Spotify/Spotify/Helpers/ColorTheme/ColorTheme.swift
@@ -0,0 +1,16 @@
+//
+//  ColorTheme.swift
+//  Spotify
+//
+//  Created by Melik Demiray on 17.12.2024.
+//
+
+import Foundation
+import UIKit
+
+enum ColorTheme {
+    static let green = UIColor(hex: "#1ED760")
+    static let gray = UIColor(hex: "#777777")
+    static let lightBlack = UIColor(hex: "#282828")
+    static let black = UIColor(hex: "#121212")
+}
--- Spotify/Spotify/Screens/Start/StarVC.swift
+++ Spotify/Spotify/Screens/Start/StarVC.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class StarVC: UIViewController {
 
+    @IBOutlet private weak var appleView: UIView!
+    @IBOutlet private weak var facebookView: UIView!
+    @IBOutlet private weak var googleView: UIView!
+    @IBOutlet private weak var welcomeLabel: UILabel!
+    @IBOutlet private weak var signUpButton: UIButton!
+    @IBOutlet private weak var logInButton: UIButton!
+
     var viewModel: StartVMProtocol! {
         didSet {
             viewModel.delegate = self
@@ -17,10 +24,97 @@ class StarVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(hex: "1B1A1C")
+        view.backgroundColor = ColorTheme.black
+        setConfigures()
 
     }
 
+    //MARK: - Configurations
+
+    private func configureLabel() {
+        welcomeLabel.text = "Millions Of Songs.\nFree On Spotify."
+    }
+
+    private func configureViews() {
+        let views = [appleView, facebookView, googleView]
+
+        for view in views {
+            guard let view = view else { continue }
+            view.layer.cornerRadius = 23
+            view.layer.borderWidth = 1
+            view.layer.borderColor = UIColor.white.cgColor
+            view.clipsToBounds = true
+            view.backgroundColor = ColorTheme.black
+
+            view.heightAnchor.constraint(equalToConstant: 46).isActive = true
+
+            let gesture = UITapGestureRecognizer(
+                target: self, action: #selector(didTapView(_:)))
+            view.addGestureRecognizer(gesture)
+            view.isUserInteractionEnabled = true
+
+        }
+    }
+
+    private func configureSignUpButton() {
+        signUpButton.layer.removeAllAnimations()
+    }
+
+    private func configureLogInButton() {
+        logInButton.layer.removeAllAnimations()
+    }
+
+    private func setConfigures() {
+        configureLabel()
+        configureViews()
+        configureSignUpButton()
+        configureLogInButton()
+    }
+
+    @objc private func didTapView(_ sender: UITapGestureRecognizer) {
+        guard let tappedView = sender.view else { return }
+
+        buttonAnimate(tappedView)
+        // Handle view-specific actions
+        switch tappedView {
+        case appleView:
+            print("Apple")
+        case facebookView:
+            print("Facebook")
+        case googleView:
+            print("Google")
+        default:
+            break
+        }
+    }
+
+    private func buttonAnimate(_ view: UIView) {
+        UIView.animate(
+            withDuration: 0.1,
+            animations: {
+                // Scale down
+                view.transform = CGAffineTransform(
+                    scaleX: 0.95, y: 0.95)
+            }
+        ) { _ in
+            UIView.animate(withDuration: 0.1) {
+                // Scale back to original size
+                view.transform = .identity
+            }
+        }
+
+    }
+
+    //MARK: - Actions
+
+    @IBAction private func signUpButtonClicked(_ sender: Any) {
+        buttonAnimate(signUpButton)
+        //TODO: Handle sign up button click
+    }
+    @IBAction private func logInButtonClicked(_ sender: Any) {
+        buttonAnimate(logInButton)
+        //TODO: Handle log in button click
+    }
 }
 
 extension StarVC: StartVMDelegate {
